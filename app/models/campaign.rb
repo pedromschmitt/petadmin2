a5c4f61431f5aadf989b1ae7bcdf8030deb03118
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-03-22 19:09:03 -0300

    Create schedule_emails

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -5,7 +5,16 @@ class Campaign < ApplicationRecord
   has_many :campaign_clients
   has_many :clients, through: :campaign_clients
 
+  after_save :schedule_emails
+
   def fae_display_field
     title
+  end
+
+  def schedule_emails
+    Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: self)
+      CampaignJob.perform_later client, self.title, self.body
+    end
   end
 end
