2e57b2a13f24bf1bb4337d9e58eac2974f7d610b
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-03-22 16:49:31 -0300

    Config Job

--- app/jobs/campaing_job.rb
+++ app/jobs/campaing_job.rb
@@ -1,7 +1,7 @@
 class CampaingJob < ApplicationJob
-  queue_as :default
+  queue_as :emails
 
-  def perform(*args)
-    # Do something later
+  def perform(client, title, body)
+    MarketingMailer.campaign(client, title, body).deliver_now
   end
 end
