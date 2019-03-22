2131c2e6a8c874125880f063e8b0b5f117504417
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-03-22 16:25:44 -0300

    Change mailcatcher port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'mailcatcher', port: 1025 }
+  config.action_mailer.smtp_settings = { address: 'mailcatcher', port: 25 }
 
   config.action_mailer.perform_caching = false
 
